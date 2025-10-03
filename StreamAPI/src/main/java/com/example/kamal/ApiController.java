6e7a96f152ed45b5e6f83db4e236f02d55a5814a
kamaldeep59 <kamalsokhal4059@gmail.com>
2025-10-03 18:54:38 +0530

    getmapping

--- StreamAPI/src/main/java/com/example/kamal/ApiController.java
+++ StreamAPI/src/main/java/com/example/kamal/ApiController.java
@@ -1,5 +1,18 @@
 package com.example.kamal;
 
-public class ApiController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ApiController {
+	
+	@GetMapping("hello")
+	public String m1() {
+		
+		return "abc";
+		
+	}
+	
+
+	
 }
